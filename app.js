cf6564fd9a78e21434df774c0e2ade3d7f29e7fa
Jaydipsinh Vaghela <jaydeep.v@productivet.com>
2017-06-14 15:33:39 +0530

    stable

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
 var routes = require('./routes');
 var passport = require('passport');
 var db = require('./lib/db');
+var logger = require('./lib/logger');
+
+logger.info('Logger test with INFO.');
+logger.error('Logger test with ERROR.');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,9 +19,9 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit:'50mb', extended: true }));
+//app.use(logger('dev'));
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit:'100mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
@@ -28,21 +32,23 @@ require('./lib/oauth')(passport);
 app.use('/', require('./lib/oauth/authorization'));
 
 app.use('/', function(req, res, next) {
-
-  if(req.headers.authorization){
+  if(req.headers.authorization) {
     db['tokenmapped'].findOne({uuid:req.headers.authorization}, function(err, user) {
       if(err) {
+        logger.error('App.js :: ' + err);
         throw err;
       }
       if(user) {
         req.headers.authorization = user.token;
       } else {
-        return res.status(403).send({success: false, msg: 'Invalid token provided.'});
+        logger.error('Invalid token provided.');
+        return res.status(403).send({success: false, message: 'Invalid token provided.'});
       }
       next();
     });
   } else {
-    return res.status(403).send({success: false, msg: 'No token provided.'});
+    logger.error('No token provided.');
+    return res.status(403).send({success: false, message: 'No token provided.'});
   }
 
 });
