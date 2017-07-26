fff5597f5eb2adcda558a0672376d0c035524a1f
prachi@minion <prachi281194@gmail.com>
2017-07-27 01:39:15 +0530

    stable upload post/get

--- routes/upload/upload.js
+++ routes/upload/upload.js
@@ -5,15 +5,44 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs-extra');
 var logger = require('../../lib/logger');
+var Jimp = require('jimp')
 
 router.post('/', function(req, res, next) {
   req.pipe(req.busboy);
   req.busboy.on('file', function (fieldname, file, filename) {
     logger.log("Uploading: " + filename);
+    // var ext = filename.substring(filename.lastIndexOf('.') + 1).toLowerCase();
+    //
+    // if(ext === 'jpg' || ext === 'png' || ext === 'gif'){
+    //
+    // }
+
     var filename = (new Date()).getTime() + '-' + filename;
     filename = decodeURI(filename);
     //Path where file will be uploaded
-    var fstream = fs.createWriteStream(process.env.FILE_STORE + '/profile/' + filename);
+    var dir = process.env.FILE_STORE + '/' + req.body.UID ;
+
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
+    var profilePhoto = dir + '/profilePhoto';
+
+    if (!fs.existsSync(profilePhoto)){
+      fs.mkdirSync(profilePhoto);
+    }
+
+    //console.log(profilePhoto);
+
+    var fstream = fs.createWriteStream(dir + '/' + filename);
+
+    Jimp.read(filename, function (err, file) {
+      if (err) throw err;
+      file.resize(256, 256)
+      .quality(50)
+      .write(profilePhoto + '/' + filename);
+    });
+
     file.pipe(fstream);
     fstream.on('close', function () {
       res.status(201).json({file: filename});
@@ -26,11 +55,8 @@ router.get('/:file', function(req, res, next) {
     res.status(422).json({'message' : 'file not provided'})
     return;
   }
-
   var filename = process.env.FILE_STORE  + '/profile/' + req.params.file;
-
   filename = decodeURI(filename);
-
   if (!fs.existsSync(filename)){
     res.status(404).json({'message' : 'file not found'})
     return;
