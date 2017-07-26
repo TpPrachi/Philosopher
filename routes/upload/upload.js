fbd628626da8043d81757d7461a13dd18a0145c1
prachi@minion <prachi281194@gmail.com>
2017-07-26 23:02:33 +0530

    stable upload post/get

--- routes/upload/upload.js
+++ routes/upload/upload.js
@@ -9,23 +9,34 @@ var logger = require('../../lib/logger');
 router.post('/', function(req, res, next) {
   req.pipe(req.busboy);
   req.busboy.on('file', function (fieldname, file, filename) {
-    console.log(file);
-    console.log(fieldname);
-    console.log(filename);
     logger.log("Uploading: " + filename);
     var filename = (new Date()).getTime() + '-' + filename;
-    console.log(filename);
     filename = decodeURI(filename);
     //Path where file will be uploaded
-    var fstream = fs.createWriteStream(process.env.FILE_STORE + filename);
-
+    var fstream = fs.createWriteStream(process.env.FILE_STORE + '/profile/' + filename);
     file.pipe(fstream);
-
     fstream.on('close', function () {
-      debug("Upload Finished of " + filename);
       res.status(201).json({file: filename});
     });
   });
+});
+
+router.get('/:file', function(req, res, next) {
+  if(!req.params.file){
+    res.status(422).json({'message' : 'file not provided'})
+    return;
+  }
+
+  var filename = process.env.FILE_STORE  + '/profile/' + req.params.file;
+
+  filename = decodeURI(filename);
+
+  if (!fs.existsSync(filename)){
+    res.status(404).json({'message' : 'file not found'})
+    return;
+  }
+  var fileToShow = filename.substring(filename.lastIndexOf('/') + 15 , filename.length);
+  res.download(filename , fileToShow)
 });
 
 module.exports = router;
