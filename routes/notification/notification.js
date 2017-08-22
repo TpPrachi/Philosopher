4db5e9d49030eab843d42b7c2f793f5061ea65e1
Jaydipsinh Vaghela <jaydeep.v@productivet.com>
2017-08-22 11:52:39 +0530

    stable

--- routes/notification/notification.js
+++ routes/notification/notification.js
@@ -16,13 +16,14 @@
   var query = require('../../lib/query');
 
   // GET api for getting notification based on filter data provided in query string
+  // We also need to provide information about users as well as philosophy for display with notification
   router.get('/', query.filter, function(req, res, next) {
-    db['notification'].find(req.filter, req.options.select || projections || {}, req.options).toArray(function(err, philosophies) {
+    db['notification'].find(req.filter, req.options.select || projections || {}, req.options).toArray(function(err, notifications) {
       if(err) {
-        logger.log(err);
+        logger.log("Error while getting notifications :: " + err);
         res.status(501).send({"success":false, "message":err});
       }
-      res.status(200).json(philosophies);
+      res.status(200).json(notifications);
     });
   });
 
