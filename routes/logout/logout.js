9da5dc34b38059c9d31c354b13f942f65e9a662a
Jaydipsinh Vaghela <jaydeep.v@productivet.com>
2017-08-24 21:36:48 +0530

    stable

---
+++ routes/logout/logout.js
@@ -0,0 +1,26 @@
+/**
+* @name routes/users/logout.js
+* @author Jaydipsinh Vaghela <jaydip.vaghela@gmail.com>
+*
+* @version 0.0.1
+*/
+
+(function(){
+  'use strict'
+  var express = require('express');
+  var router = express.Router();
+  var db = require('../../lib/db');
+  var logger = require('../../lib/logger')(__filename);
+
+
+  router.delete('/', function(req, res, next) {
+    db['tokenmapped'].findOneAndDelete({'uuid': req.headers['authorization']}, function(err, deleted) {
+      if(err) {
+        res.status(501).send({success:false, message:"Error while removing mapping from token mapped collection."});
+      }
+      res.send(201).send({success:true, message:"User logout successfully."});
+    });
+  });
+
+  module.exports = router;
+})();
