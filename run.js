795c78392761b647efbd456a3ffdce93ece73c96
Jaydipsinh Vaghela <jaydeep.v@productivet.com>
2017-09-12 11:18:49 +0530

    stable

--- run.js
+++ run.js
@@ -3,35 +3,24 @@
 (() => {
   const _ = require('lodash');
 
-  trem(50000, function(d,n){
-    console.log(d);
-    console.log(n);
-  });
-
-  function trem(n, cb) {
-    function rec(n,k, cb) {
-        if(k <= 1) {
-          n += 1;
-          cb(n);
-          return null;
-        } else {
-          return function(){
-              n = n+k
-              return rec.bind(this, n, k-1, cb);
-          };
-        }
+  function findFactorial(n, cb) {
+    var op = rec(n, n-1, cb);
+    while (op != null && typeof op === 'function') {
+      op = op();
     }
-
-    return rec.bind(this, n, n-1, cb);
   }
 
+  function rec(n,k, cb) {
+      if(k === 0) {
+        cb(n);
+      } else {
+        n += k
+        return rec.bind(this, n, k-1, cb);
+      }
+  }
 
-
+  const findRec = 500000;
+  findFactorial(findRec, function(vv){
+    console.log("VV :: " + vv);
+  });
 })();
-
-// For Big CSV file Conversion
-//var Converter=require("csvtojson").Converter;
-//var csvConverter=new Converter({constructResult:false}); // The parameter false will turn off final result construction. It can avoid huge memory consumption while parsing. The trade off is final result will not be populated to end_parsed event.
-//var readStream=require("fs").createReadStream("inputData.csv");
-//var writeStream=require("fs").createWriteStream("outpuData.json");
-//readStream.pipe(csvConverter).pipe(writeStream);
