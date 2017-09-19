c182482274e6332e0214a401e9405c4e37f331a5
Jaydipsinh Vaghela <jaydeep.v@productivet.com>
2017-09-19 18:30:23 +0530

    stable

--- routes/users/softSchema.js
+++ routes/users/softSchema.js
@@ -15,6 +15,7 @@ var schema = {
   lastname: string.label('Last Name'),
   email: string.label('Email').email(),
   fullname : string.label('Full Name'),
+  username : string.label('User Name'),
   dateOfBirth : date.label('Date Of Birth'),
   gender : string.label('Gender'),
   biolosophy : bigString.label('Biolosophy'),
