a5e62983f8a54b783308227ccd3d06f3084c1211
Jaydipsinh Vaghela <jaydeep.v@productivet.com>
2017-09-27 13:15:36 +0530

    stable

--- routes/users/users.js
+++ routes/users/users.js
@@ -128,30 +128,15 @@
     var patch = req.body;
     patch["UpdatedDate"] = new Date();
 
-    var checkValidityForFullname = true;
-    var checkValidityForEmail = true;
-
     db['users'].find({_id: db.ObjectID(req.params.id)}).toArray(function(err, users) {
       if(err){
         logger.error(err);
         res.status(501).send({"success":false, "message":err});
       }
 
-      if (users && users[0] && users[0].fullname) {
-        if (patch && patch.fullname && patch.fullname ===  users[0].fullname) {
-          checkValidityForFullname = false;
-          res.status(501).send({"success":false, "message": "Patching the same fullname, Please provide valid data for perform operation."});
-        }else {
-          checkValidityForFullname = true;
-        }
-      }
-
       if (users && users[0] && users[0].email) {
         if (patch && patch.email && patch.email ===  users[0].email) {
-          checkValidityForEmail = false;
           res.status(501).send({"success":false, "message": "Patching the same email, Please provide valid data for perform operation."});
-        }else {
-          checkValidityForEmail = true;
         }
       }
 
@@ -179,11 +164,7 @@
           res.status(200).send({"success":true, "message":data.value});
         });
       });
-      // Remove password fields from return object
-      //res.status(200).json(data);
     });
-
-    // Need to change here
 
   });
 
